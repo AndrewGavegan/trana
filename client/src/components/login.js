41e4942943b8f3e1e7b6ae10c41eafd484d49203
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-08-02 21:14:20 +1000

    adding user login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,16 +1,74 @@
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import auth from '../utils/auth';
 
-export default function Login() {
-  return (
-    <Form className="loginForm">
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter your password" />
-      </Form.Group>
-    </Form>
-  )
+export default function Login(props) {
+
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      email: '',
+      password: '',
+    });
+
+    return (
+      <div>
+        {data ? (
+          <p>
+            You've logged in successfully
+          </p>
+        ) : (
+          <Form onSubmit={handleFormSubmit} className="loginForm">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email address"
+                name="email"
+                value={formState.email}
+                onChange={handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                name="password"
+                value={formState.password}
+                onChange={handleChange} />
+            </Form.Group>
+            <Button
+              type="submit"
+            >Login</Button>
+          </Form>)}
+      </div>
+    )
+  }
 }
