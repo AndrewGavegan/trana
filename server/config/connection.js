ef0df1ff53dd16fef8a7ea8dc79d7f9350137091
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-08-04 12:06:31 +1000

    deploying first try

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://AndrewGavegan:password1234@cluster0.omy9u.mongodb.net/?retryWrites=true&w=majority' || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/trana', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/trana', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
+
+
+// 'mongodb+srv://AndrewGavegan:password1234@cluster0.omy9u.mongodb.net/?retryWrites=true&w=majority' ||
